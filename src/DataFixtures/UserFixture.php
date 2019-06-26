7e1b52e073a4aa82a3af59c23883d274651bc330
manueljun17 <manueljun17@gmail.com>
2019-06-26 14:55:26 +0800

    Adding & Checking the User's Password

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,8 +4,17 @@ namespace App\DataFixtures;
 use App\Entity\User;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 class UserFixture extends BaseFixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'main_users', function($i) {
@@ -13,6 +22,10 @@ class UserFixture extends BaseFixture
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
 
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'engage'
+            ));
             return $user;
         });
 
