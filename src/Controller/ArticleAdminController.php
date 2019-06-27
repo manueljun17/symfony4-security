531b3d926bf2b6d89dbdd0324b9993fc89e71f0b
manueljun17 <manueljun17@gmail.com>
2019-06-27 16:31:10 +0800

    Dynamic Roles

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -7,11 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class ArticleAdminController extends AbstractController
 {
 	/**
-     * @Route("/admin/article/new")
+     * @Route("/admin/article/new", name="admin_article_new")
      */
 	public function new(EntityManagerInterface $em)
     {
