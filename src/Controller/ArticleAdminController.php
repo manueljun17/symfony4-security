f178ed22c2d7da25b4dec9e9eda15db1bd67de93
manueljun17 <manueljun17@gmail.com>
2019-07-10 15:34:01 +0800

    Adding a Custom Voter

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -29,12 +29,10 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-         if (!$this->isGranted('MANAGE', $article)) {
-            throw $this->createAccessDeniedException('No access!');
-        }
         dd($article);
     }
 
